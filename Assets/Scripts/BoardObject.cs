2a4e510c355f62885450cd3cbc582831471c188c
Salvador Melo <salmelo16@gmail.com>
2019-04-27 19:15:21 -0400

    tilemap graphics

--- Assets/Scripts/BoardObject.cs
+++ Assets/Scripts/BoardObject.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 public class BoardObject
 {
     public Board Board { get; }
+    public Vector2Int Position => Board.GetPosition(this);
 
     public BoardObject(Board board)
     {
